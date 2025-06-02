94bbbb963175097cb0579bb6dd004a5c154716a5
serght <sergioherrerat@javeriana.edu.co>
2025-06-01 21:14:15 -0500

    se arreglo el auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8081/auth', 
+  baseURL: 'http://localhost:8081/auth',
 });
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) {
+  
+  
+  if (token && !config.url.endsWith('/login') && !config.url.endsWith('/register')) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  
   return config;
 });
+
 export const login = (credentials) => API.post('/login', credentials);
 export const register = (data) => API.post('/register', data);
